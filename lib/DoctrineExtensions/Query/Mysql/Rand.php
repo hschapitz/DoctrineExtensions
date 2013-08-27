e321631aa58c213beb9c068fa212395872655f6e
agentor <agentor@gmx.de>
2013-08-27 20:21:52 +0200

    MySQL RAND() function

---
+++ lib/DoctrineExtensions/Query/Mysql/Rand.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 
+ * @see https://gist.github.com/Ocramius/919465
+ *
+ */
+namespace DoctrineExtensions\Query\Mysql;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode,
+    Doctrine\ORM\Query\Lexer;
+
+/**
+ * RandFunction ::= "RAND" "(" ")"
+ */
+class Rand extends FunctionNode
+{
+ 
+    public function parse(\Doctrine\ORM\Query\Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+ 
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
+    {
+        return 'RAND()';
+    }
+}
